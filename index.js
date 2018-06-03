b1bbd0d1bbf013fba4ecba79f7c87d0539f0c44d
rknruben56 <rodrigrd.rosehulman@gmail.com>
2018-06-03 13:23:45 -0500

    added HTTP GET and POST methods for user table

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
-'use strict';
 const doc = require('dynamodb-doc');
 const dynamo = new doc.DynamoDB();
-
-const UserHandler = require('./handlers/Users/UserHandler');
-const LocationHandler = require('./handlers/location/LocationHandler');
-const RequestHandler = require('./handlers/request/RequestHandler');
 const logger = require('./logging/Logger');
 
 /**
- * Returns an HTTP 400 response with the error message
- * @param {*} errorMessage 
- * @param {*} httpCallback 
+ * Function that returns the HTTP response
+ * @param {object} err 
+ * @param {object} res 
+ * @param {function} callback 
  */
-let returnErrorResponse = (errorMessage, httpCallback) => {
-    let httpResponse = {
-        statusCode: 400,
-        body: errorMessage,
+const done = (err, res, callback) => {
+    callback(null, {
+        statusCode: err ? 400 : 200,
+        body: err ? err.message : JSON.stringify(res),
         headers: {
             'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*', // Needed for lambdas to talk to each other
+            'Access-Control-Allow-Credentials': true // Needed for HTTPS cookie stuff
         }
-    };
-
-    httpCallback(null, httpResponse);
+    });
 };
 
 module.exports = {
 
     /**
-     * /users
+     * Get all users
      */
     getUsers: (event, context, callback) => {
-        // TODO: Extract out for the future RESTful methods
-        const done = (err, res) => callback(null, {
-            statusCode: err ? 400 : 200,
-            body: err ? err.message : JSON.stringify(res),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
+        logger.info('Received event for getUsers: ' + JSON.stringify(event));
+        dynamo.scan({TableName: "User"}, (err, res) => done(err, res, callback));
+    },
 
-        dynamo.scan({TableName: "User"}, done);
-    }    
+    /**
+     * Add/Update user
+     */
+    updateUser: (event, context, callback) => {
+        logger.info('Received event for updateUser: ');
+        logger.info(event);
+        let body = typeof event.body === "string" ? JSON.parse(event.body) : event.body;
+        let params = {};
+        params.TableName = "User";
+        params.Key = {id: body.userID};        
+        dynamo.updateItem(params, (err, res) => done(err, res, callback));
+    }
 }
